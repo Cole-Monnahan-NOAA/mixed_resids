db1df4bf2632b9b73848bdbe5fbb5d98de6cf490
Cole-Monnahan-NOAA <cole.monnahan@noaa.gov>
2020-12-21 14:50:58 -0800

    Rework random walk model to do conditional and unconditional RE at the same time

--- models/randomwalk.cpp
+++ models/randomwalk.cpp
@@ -23,18 +23,32 @@ Type objective_function<Type>::operator()()
 
   // Initial condition
   Type nll = -dnorm(x(0), Type(0), huge, true);
+  // This is for the unconditional DHARMa residuals
+  vector<Type> x2(x.size());
+  vector<Type> y2(x.size());
+
   // Increments
+  SIMULATE{
+    x2(0)=rnorm(Type(0) ,exp(logsigma));
+  }
   for (int i = 1; i < x.size(); ++i){
     nll -= dnorm(x(i), x(i - 1) + mu, exp(logsigma), true);
+    SIMULATE {
+      x2(i) = rnorm(x2(i-1)+mu, exp(logsigma));
+    }
   }
   // Observations
   for (int i = 0; i < y.size(); ++i){
     nll -= keep(i) * dnorm(y(i), x(i), exp(logs), true);
     SIMULATE {
-      y(i) = rnorm(x(i), exp(logs));
+      y(i) = rnorm(x(i), exp(logs)); // conditional
+      y2(i)=rnorm(x2(i), exp(logs)); // unconditional
     }
   }
+  REPORT(x);
   REPORT(y);
-    
+  REPORT(x2);
+  REPORT(y2);
+ 
   return nll;
 }
