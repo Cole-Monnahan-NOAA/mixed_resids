b376a3b3cc3918ca0a6a4772056de50d597002e0
Cole-Monnahan-NOAA <cole.monnahan@noaa.gov>
2021-07-12 08:58:27 -0700

    Fix bug in simpleGLMM model

--- models/simpleGLMM.cpp
+++ models/simpleGLMM.cpp
@@ -18,32 +18,36 @@ Type objective_function<Type>::operator()()
 
   Type cdf;
 
+  // Probability of random effects
   vector<Type> mu(y.size());
   for(int j=0; j<u.size(); j++){
     nll -= dnorm(u(j), Type(0), sig_u, true);
     if(sim_re == 1){
-       SIMULATE{
-       u(j) = rnorm(Type(0), sig_u);
-     }
-    }
-    
-    for(int i=0; i<y.size(); i++){
-      mu(i) = b0 + u(group(i));
-      nll -= keep(i) * dnorm(y(i), mu(i), sig_y, true);
-      cdf = squeeze(pnorm(y(i),  mu(i), sig_y ));
-      nll -= keep.cdf_lower(i) * log( cdf );
-      nll -= keep.cdf_upper(i) * log( 1.0 - cdf );
       SIMULATE{
-        y(i) = rnorm(mu(i), sig_y);
+	u(j) = rnorm(Type(0), sig_u);
       }
     }
   }
+
+  // Likelihood
+  for(int i=0; i<y.size(); i++){
+    mu(i) = b0 + u(group(i));
+    nll -= keep(i) * dnorm(y(i), mu(i), sig_y, true);
+    cdf = squeeze(pnorm(y(i),  mu(i), sig_y ));
+    nll -= keep.cdf_lower(i) * log( cdf );
+    nll -= keep.cdf_upper(i) * log( 1.0 - cdf );
+    SIMULATE{
+      y(i) = rnorm(mu(i), sig_y);
+    }
+  }
+  
   SIMULATE{
     REPORT(y);
     if(sim_re == 1){
       REPORT(u);
     }
   }
+  
   REPORT(mu);
   REPORT(u);
   REPORT(sig_u);
