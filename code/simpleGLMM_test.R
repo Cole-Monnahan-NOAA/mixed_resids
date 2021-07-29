dba2f7b220848a48e2152dce03ee406040303b88
Cole-Monnahan-NOAA <cole.monnahan@noaa.gov>
2021-07-29 13:11:41 -0700

    Add partial conditional method to simpleGLMM test

--- code/simpleGLMM_test.R
+++ code/simpleGLMM_test.R
@@ -32,7 +32,7 @@ print(checkConsistency(obj))
 
 ## Now quick simulation testing of pvalues
 nsim <- 5000
-uncond.pval <- cond.pval <- fg.pval <- posttruth.pval <- postmle.pval <- rep(0,nsim)
+parcond.pval <- parcond2.pval <- uncond.pval <- cond.pval <- fg.pval <- posttruth.pval <- postmle.pval <- rep(0,nsim)
 for(i in 1:nsim){
   out <- simulate.simpleGLMM(i, ngroups=n.groups, nobs=n.obs)
   obj <- MakeADFun(out$Data, out$Par, random = "u", DLL = "simpleGLMM", silent = TRUE)
@@ -57,37 +57,65 @@ for(i in 1:nsim){
   resids.uncond <- residuals(dharma.uncond, quantileFunction = qnorm,
                              outlierValues = c(-7,7))
   uncond.pval[i] <- ad.test(resids.uncond, null = 'pnorm', estimated = TRUE)$p.value[[1]]
-  ## Use posterior draws via tmbstan, after fixing the FE at
-  ## either the truth or the MLE
+  ## Partial conditioned in two ways. First is simulating from
+  ## full joint posterior (i.e. w/ fixed effects)
+  sdr <-  sdreport(obj, getJointPrecision=TRUE)
+  dat <- out$Dat
+  obj$env$data$simRE <- 0 # turn off RE simulation
+  joint.mle <- obj$env$last.par.best
+  tmp <- replicate(1000, {
+        newpar <- rmvnorm_prec(mu=joint.mle, prec=sdr$jointPrecision)
+        obj$simulate(par=newpar)[['y']]
+  })
+  dharma <- createDHARMa(tmp, dat$y, fittedPredictedResponse=rep$mu)
+  resids <- residuals(dharma, quantileFunction = qnorm, outlierValues = c(-7,7))
+  parcond.pval[i] <- goftest::ad.test(resids,'pnorm', estimated = TRUE)$p.value
+  ## To drop the fixed effects I Think I need to rebuild the obj
+  ## with them fixed
   map <- list(b0=factor(NA), ln_sig_u=factor(NA), ln_sig_y=factor(NA))
-  pars <- list(b0=4, ln_sig_u=log(sqrt(out$sig2[2])), ln_sig_y=log(sqrt(out$sig2[1])), u=rep(0, n.groups))
-  objtruth <- MakeADFun(out$Data, parameters=pars, map = map)
-  fittruth <- tmbstan(objtruth, chains=1, warmup=500, iter=501, refresh=-1)
-  ## Take single posterior sample
-  posttruth <- as.numeric(as.matrix(fittruth))
-  Fx <- pnorm(q=out$Data$y,
-              mean=pars$b0+ posttruth[out$Data$group+1],
-              sd=exp(pars$ln_sig_y))
-  residual <- qnorm(Fx)
-  posttruth.pval[i] <- ad.test(residual, null='pnorm', estimated=TRUE)$p.value[1]
-  ## Redo but use the MLE values
   pars <- c(as.list(opt$par)); pars$u <- rep(0, len=n.groups)
-  objmle <- MakeADFun(out$Data, parameters=pars, map = map)
+  objmle <- MakeADFun(out$Data, parameters=pars, map = map, silent=TRUE)
+  objmle$env$data$simRE <- 0 # turn off RE simulation
+  optmle <- nlminb(objmle$par, objmle$fn, objmle$gr)
+  sdrmle <- sdreport(objmle)
+  joint.mle <- objmle$env$last.par.best
+  tmp <- replicate(1000, {
+    newpar <- mvtnorm::rmvnorm(1, mean=joint.mle, sigma=sdrmle$cov.fixed)
+    objmle$simulate(par=newpar)[['y']]
+  })
+  dharma <- createDHARMa(tmp, dat$y, fittedPredictedResponse=rep$mu)
+  resids <- residuals(dharma, quantileFunction = qnorm, outlierValues = c(-7,7))
+  parcond2.pval[i] <- goftest::ad.test(resids,'pnorm', estimated = TRUE)$p.value
+  ## Use posterior draws via tmbstan, after fixing the FE at
+  ## either MLE or truth
+  ## start with the MLE one
   fitmle <- tmbstan(objmle, chains=1, warmup=500, iter=501, refresh=-1)
-  ## Take single posterior sample
   postmle <- as.numeric(as.matrix(fitmle))
   Fx <- pnorm(q=out$Data$y,
               mean=pars$b0+ postmle[out$Data$group+1],
               sd=exp(pars$ln_sig_y))
   residual <- qnorm(Fx)
   postmle.pval[i] <- ad.test(residual, null='pnorm', estimated=TRUE)$p.value[1]
+  ## now the truth
+  pars <- list(b0=4, ln_sig_u=log(sqrt(out$sig2[2])), ln_sig_y=log(sqrt(out$sig2[1])), u=rep(0, n.groups))
+  objtruth <- MakeADFun(out$Data, parameters=pars, map = map)
+  fittruth <- tmbstan(objtruth, chains=1, warmup=500, iter=501, refresh=-1)
+  posttruth <- as.numeric(as.matrix(fittruth))
+  Fx <- pnorm(q=out$Data$y,
+              mean=pars$b0+ posttruth[out$Data$group+1],
+              sd=exp(pars$ln_sig_y))
+  residual <- qnorm(Fx)
+  posttruth.pval[i] <- ad.test(residual, null='pnorm', estimated=TRUE)$p.value[1]
 }
 
 pvals <-
-  tibble(fg.pval, cond.pval, uncond.pval, posttruth.pval,
+  tibble(fg.pval, parcond.pval, parcond2.pval, cond.pval, uncond.pval, posttruth.pval,
                 postmle.pval) %>%
   pivot_longer(cols=everything(), names_to='method',
                 values_to='pvalue') %>%
                 mutate(method=gsub('.pval', '', method))
-ggplot(pvals, aes(pvalue)) + facet_wrap('method') + geom_histogram(bins=20)
+g <- ggplot(pvals, aes(pvalue)) + facet_wrap('method') +
+  geom_histogram(bins=20)
+ggsave('plots/simpleGLMM_test.png', g, width=7, height=5)
+
 
