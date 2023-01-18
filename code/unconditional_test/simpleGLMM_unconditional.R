9e378ae46267701c7bebeb387cee5871793bb9de
Andrea-Havron-NOAA <andrea.havron@noaa.gov>
2023-01-18 13:52:01 -0800

    update gamma simulations and add rotation based on RE

--- code/unconditional_test/simpleGLMM_unconditional.R
+++ code/unconditional_test/simpleGLMM_unconditional.R
@@ -10,14 +10,16 @@ library(tidyr)
 n <- 100
 ng <- 5
 sd.u <- 1
+beta <- c(2.3, 0.4)
 
 #Simulate approx normal ([1]) and skewed ([2]) gamma
-y.var <- c(.001, 1)
+y.var <- c(1, 4.5)
 
 n.sim <- 100
 pvals.gamma.1 <- pvals.gamma.2 <-
   data.frame(uncond.rot = rep(NA, n.sim),
              uncond.nrot = rep(NA, n.sim),
+             uncond.re.rot = rep(NA, n.sim),
              cond.nrot = rep(NA, n.sim) )
 
 for(ii in 1:n.sim){
@@ -26,61 +28,77 @@ for(ii in 1:n.sim){
   
 ## Obs data =================================
   # Approximately normal
-  y.gamma.1 <- as.matrix(u) %>% exp() %>%
+  y.gamma.1 <- as.matrix(u + beta[1]) %>% exp() %>%
     apply(., 1, function(x) rgamma(n, x^2/y.var[1], scale = y.var[1]/x)) %>% 
     as.vector() 
   # Positively skewed
-  y.gamma.2 <- as.matrix(u) %>% exp() %>%
+  y.gamma.2 <- as.matrix(u + beta[2]) %>% exp() %>%
     apply(., 1, function(x) rgamma(n, x^2/y.var[2], scale = y.var[2]/x)) %>% 
     as.vector() 
  
 ## Unconditional Simulation ================
-
-  sim.gamma.1 <- replicate(1000, {
-    rnorm(ng, 0, sd=sd.u) %>%
-      as.matrix() %>% exp() %>%
-      apply(., 1, function(x) rgamma(n, x^2/y.var[1], scale = y.var[1]/x)) %>%
-      as.vector()
+  sim.norm.1 <- replicate(1000, {
+    rep(rnorm(ng, beta[1], sd=sd.u), each = n)
   })
-  sim.gamma.2 <- replicate(1000, {
-    rnorm(ng, 0, sd=sd.u) %>% exp() %>%
-      as.matrix() %>%
-      apply(., 1, function(x) rgamma(n, x^2/y.var[2], scale = y.var[2]/x)) %>%
-      as.vector() 
+  sim.gamma.1 <- sim.norm.1 %>%
+      exp() %>%
+      apply(., 2, function(x) rgamma(n*ng, x^2/y.var[1], scale = y.var[1]/x))
+  
+  sim.norm.2 <- replicate(1000, {
+    rep(rnorm(ng, beta[2], sd=sd.u), each = n)
   })
+  sim.gamma.2 <- sim.norm.2 %>%
+    exp() %>%
+    apply(., 2, function(x) rgamma(n*ng, x^2/y.var[2], scale = y.var[2]/x))
   
   # Calculate p-values ===================
-  ## Rotated
+  ## Rotated - estimated using covariance of simulated data
   pvals.gamma.1$uncond.rot[ii] <- 
     ks.test(
       createDHARMa(sim.gamma.1, y.gamma.1,
                    rotation = "estimated")$scaledResiduals, 
       'punif')$p.value
-  pvals.gamma.2$uncond.rot[ii] <- 
+  pvals.gamma.2$uncond.rot[ii] <-
     ks.test(
       createDHARMa(sim.gamma.2, y.gamma.2,
-                   rotation = "estimated")$scaledResiduals, 
+                   rotation = "estimated")$scaledResiduals,
       'punif')$p.value
+  
+  ## Rotated - estimated using covariance of simulated RE
+  covar.1 <- Matrix::nearPD(cov(t(sim.norm.1)))$mat
+  pvals.gamma.1$uncond.re.rot[ii] <- 
+    ks.test(
+      createDHARMa(sim.gamma.1, y.gamma.1,
+                   rotation = as.matrix(covar.1))$scaledResiduals,
+      'punif')$p.value
+  covar.2 <- Matrix::nearPD(cov(t(sim.norm.2)))$mat
+  pvals.gamma.2$uncond.re.rot[ii] <- 
+    ks.test(
+      createDHARMa(sim.gamma.2, y.gamma.2,
+                   rotation = as.matrix(covar.2))$scaledResiduals,
+      'punif')$p.value
+  
   
   ## Not Rotated
   pvals.gamma.1$uncond.nrot[ii] <- 
     ks.test(
       createDHARMa(sim.gamma.1, y.gamma.1)$scaledResiduals, 
       'punif')$p.value
-  pvals.gamma.2$uncond.nrot[ii] <- 
+  pvals.gamma.2$uncond.nrot[ii] <-
     ks.test(
-      createDHARMa(sim.gamma.2, y.gamma.2)$scaledResiduals, 
+      createDHARMa(sim.gamma.2, y.gamma.2)$scaledResiduals,
       'punif')$p.value
+  
   
   
 # Conditional Simulation ==============================
   sim.gamma.1 <- replicate(1000, {
-    as.matrix(u)  %>% exp() %>%
+    as.matrix(u + beta[1])  %>% exp() %>%
       apply(., 1, function(x) rgamma(n, x^2/y.var[1], scale = y.var[1]/x)) %>%
       as.vector()
   })
   sim.gamma.2 <- replicate(1000, {
-    as.matrix(u)%>% exp() %>%
+    as.matrix(u + beta[2])%>% exp() %>%
       apply(., 1, function(x) rgamma(n, x^2/y.var[2], scale = y.var[2]/x)) %>%
       as.vector()
   })
@@ -91,9 +109,9 @@ for(ii in 1:n.sim){
     ks.test(
       createDHARMa(sim.gamma.1, y.gamma.1)$scaledResiduals, 
       'punif')$p.value
-  pvals.gamma.2$cond.nrot[ii] <- 
+  pvals.gamma.2$cond.nrot[ii] <-
     ks.test(
-      createDHARMa(sim.gamma.2, y.gamma.2)$scaledResiduals, 
+      createDHARMa(sim.gamma.2, y.gamma.2)$scaledResiduals,
       'punif')$p.value
 }
 
@@ -106,3 +124,5 @@ pvals.gamma.2 %>%
   pivot_longer(., col = 1:4, names_to = "method", values_to = "pval") %>% 
   ggplot(., aes(x=pval)) + geom_histogram() + facet_wrap(~method)
 
+save(pvals.gamma.1, file = "code/unconditional_test/pvals_gamma_1.RData")
+save(pvals.gamma.2, file = "code/unconditional_test/pvals_gamma_2.RData")
